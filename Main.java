084a5aa64595c4ab2c8b938df6d738739ff8450e
MisaelMhoron <31193185+MisaelMhoron@users.noreply.github.com>
2018-12-06 12:01:43 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enmaf;
+
+import java.io.File;
+
+/**
+ *
+ * @author missa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     JFlex.Main.generate(new File("C:/Users/missa/Documents/NetBeansProjects/backup/ENMAF/src/enmaf/Scanner.jflex"));
+}}
+    
