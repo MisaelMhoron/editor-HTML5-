cea6515e9a6c7e771c3c8612aae7858c81342af3
MisaelMhoron <31193185+MisaelMhoron@users.noreply.github.com>
2018-12-06 12:00:06 -0600

    Create simbolo.java

---
+++ simbolo.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enmaf;
+
+/**
+ *
+ * @author missa
+ */
+public class simbolo {
+  public static final int EOF = 0;
+  public static final int error = 1;
+  public static final int PCOMA =2;
+  public static final int IGUAL = 3;
+  public static final int llaves =4;
+  public static final int coma=5;
+  public static final int parent=6;
+  public static final int numero = 7;
+  public static final int tstring = 8;
+  public static final int iden = 9;
+  public static final int comentarios=10;
+  public static final int reservadasphp=11;
+  public static final int estrucont=12;
+  public static final int operadores=13;
+  public static final int finalesydemas=14;
+  public static final int operadoresphp=15;
+  public static final int esp=16;
+  public static final int otros=17;
+  public static final int htag=18;
+  public static final int estdoc=19;
+  public static final int esttxt=20;
+  public static final int phpstring = 21;
+  public static final int listas=22;
+  public static final int txtformat=23;
+  public static final int htmlcomment=24;
+  public static final int forms=25;
+  public static final int imglink=26;
+  public static final int add=27;
+  public static final int nume=28;
+  
+
+}
